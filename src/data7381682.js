55597157af01f6d7166bc0285a88a6d5e071364f
Sakshi Singh Dangi <78265680+sakshisingh12@users.noreply.github.com>
2022-04-12 13:02:32 +0530

    Update data7381682.js

--- src/data7381682.js
+++ src/data7381682.js
@@ -1,57 +1,134 @@
-const data = [
-    {
+const data = [{
         id: 1,
-        author_name: "Inderjeet Singh",
-        author_link: "https://twitter.com/3nc0d3dGuY",
-        writeup_link: "https://hackerone.com/reports/163723",
+        author_name: "Sandeep Hodkasia",
+        author_link: "https://hackerone.com/sandeep_hodkasia",
+        writeup_link: "https://hackerone.com/reports/314808",
         writeup_name: "Full Account Takeover",
         bug_name: "OAuth Flow",
-        bug_link: "https://hackerone.com/hacktivity"
+        bug_link: "https://developers.facebook.com/docs/facebook-login/security",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
     },
     {
         id: 2,
-        author_name: "Inderjeet Singh",
-        author_link: "https://twitter.com/3nc0d3dGuY",
-        writeup_link: "https://hackerone.com/reports/163723",
-        writeup_name: "Full Account Takeover",
-        bug_name: "OAuth Flow",
-        bug_link: "https://hackerone.com/hacktivity"
+        author_name: "Abdellah Yaala",
+        author_link: "https://medium.com/@yaala",
+        writeup_link: "https://medium.com/@yaala/trim-private-live-videos-and-access-them-a331447cc82a",
+        writeup_name: "Trim Private Live Videos",
+        bug_name: "GraphQL Issue",
+        bug_link: "https://fauna.com/blog/best-practices-testing-graphql-apis",
+        source_link: "https://medium.com",
+        source_name: "Medium"
     },
     {
         id: 3,
-        author_name: "Inderjeet Singh",
-        author_link: "https://twitter.com/3nc0d3dGuY",
-        writeup_link: "https://hackerone.com/reports/163723",
-        writeup_name: "Full Account Takeover",
-        bug_name: "OAuth Flow",
-        bug_link: "https://hackerone.com/hacktivity"
+        author_name: "Alex Birsan",
+        author_link: "https://medium.com/@alex.birsan",
+        writeup_link: "https://medium.com/@alex.birsan/the-bug-that-exposed-your-paypal-password-539fc2896da9",
+        writeup_name: "Exposed Paypal Password",
+        bug_name: "CSRF and XSSI",
+        bug_link: "https://portswigger.net/web-security/csrf/tokens",
+        source_link: "https://medium.com",
+        source_name: "Medium"
     },
     {
         id: 4,
-        author_name: "Inderjeet Singh",
-        author_link: "https://twitter.com/3nc0d3dGuY",
-        writeup_link: "https://hackerone.com/reports/163723",
-        writeup_name: "Full Account Takeover",
-        bug_name: "OAuth Flow",
-        bug_link: "https://hackerone.com/hacktivity"
+        author_name: "Mosec",
+        author_link: "https://medium.com/@moSec",
+        writeup_link: "https://medium.com/@moSec/how-i-hacked-thousand-of-subdomains-6aa43b92282c",
+        writeup_name: "How I Hacked Thousands of Subdomains",
+        bug_name: "Subdomain Takeover",
+        bug_link: "https://www.hackerone.com/application-security/guide-subdomain-takeovers",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
     },
     {
         id: 5,
-        author_name: "Inderjeet Singh",
-        author_link: "https://twitter.com/3nc0d3dGuY",
-        writeup_link: "https://hackerone.com/reports/163723",
-        writeup_name: "Full Account Takeover",
-        bug_name: "OAuth Flow",
-        bug_link: "https://hackerone.com/hacktivity"
+        author_name: "Vladimir Volfovich",
+        author_link: "https://hackerone.com/zhirinovskiy",
+        writeup_link: "https://hackerone.com/reports/1439026",
+        writeup_name: "Discoverability by Phone Number/Email",
+        bug_name: "Username Enumeration",
+        bug_link: "https://www.rapid7.com/blog/post/2017/06/15/about-user-enumeration/",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
     },
     {
         id: 6,
-        author_name: "Inderjeet Singh",
-        author_link: "https://twitter.com/3nc0d3dGuY",
-        writeup_link: "https://hackerone.com/reports/163723",
-        writeup_name: "Full Account Takeover",
-        bug_name: "OAuth Flow",
-        bug_link: "https://hackerone.com/hacktivity"
+        author_name: "trieulieuf9",
+        author_link: "https://hackerone.com/trieulieuf9?type=user",
+        writeup_link: "https://hackerone.com/reports/1423704",
+        writeup_name: "Detect if user is FetLife supporter",
+        bug_name: "Response Manipulation",
+        bug_link: "rashahacks.com",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
+    },
+    {
+        id: 7,
+        author_name: "Souhaib Naceri",
+        author_link: "https://hackerone.com/h4x0r_dz?type=user",
+        writeup_link: "https://hackerone.com/reports/1122513",
+        writeup_name: "Stored XSS devicelock.com/forum/",
+        bug_name: "Stored XSS",
+        bug_link: "https://portswigger.net/web-security/cross-site-scripting/stored",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
+    },
+    {
+        id: 8,
+        author_name: "Ron Chan",
+        author_link: "https://hackerone.com/ngalog",
+        writeup_link: "https://hackerone.com/reports/1102546",
+        writeup_name: "Publicly Listed Google Bucket",
+        bug_name: "Sensitive Data Exposure",
+        bug_link: "https://www.acunetix.com/blog/web-security-zone/sensitive-data-exposure/",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
+    },
+    {
+        id: 9,
+        author_name: "Danish",
+        author_link: "https://hackerone.com/danishalkatiri",
+        writeup_link: "https://hackerone.com/reports/1092831",
+        writeup_name: "Password Reset Token Leak",
+        bug_name: "Sensitive Key Leak",
+        bug_link: "rashahacks.com",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
+    },
+    {
+        id: 10,
+        author_name: "Rohan Devikar",
+        author_link: "https://hackerone.com/blackfly_",
+        writeup_link: "https://hackerone.com/reports/1411363",
+        writeup_name: "No Length on Password",
+        bug_name: "Long Password DOS",
+        bug_link: "https://github.com/fnando/password_strength/issues/27",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
+    },
+    {
+        id: 11,
+        author_name: "Misa G.",
+        author_link: "https://hackerone.com/bepresent?type=user",
+        writeup_link: "https://hackerone.com/reports/1410459",
+        writeup_name: "Reflected XSS *.shopifycloud.com",
+        bug_name: "Reflected XSS",
+        bug_link: "https://portswigger.net/web-security/cross-site-scripting/reflected",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
+    },
+    {
+        id: 12,
+        author_name: "Dhivish Varshan",
+        author_link: "https://hackerone.com/official_dhivish?type=user",
+        writeup_link: "https://hackerone.com/reports/1348504",
+        writeup_name: "Subdomain Takeover",
+        bug_name: "Subdomain Takeover",
+        bug_link: "https://www.hackerone.com/application-security/guide-subdomain-takeovers",
+        source_link: "https://hackerone.com/hacktivity",
+        source_name: "Hackerone"
     }
 ]
 
