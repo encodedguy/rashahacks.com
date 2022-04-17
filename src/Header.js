31ffe2f8faf6fce755c2876bbb36d98548fa49fb
Inderjeet Singh <singhinder1208@gmail.com>
2022-04-17 16:18:10 +0530

    Completed

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(){
     return <div class="top">
                 <div class="top-heading">
-                    <a href="https://rashahacks.com"><h1>BUG BOUNTY WRITEUPS</h1></a>
+                    <a href="https://rashahacks.com"><h1>BUG REPORTS HUB</h1></a>
                 </div>
             </div>
 }
